fd23765aa8b5804fecf0ea9b9c5f97cb253edbee
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-06-07 08:46:18 +0200

    updated setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='3ml_utils',
+    version='0.1',
+    description='Provides some additional 3ML features that may be added later',
+    author='J. Michael Burgess',
+    author_email='jmichaelburgess@gmail.com',
+    license='GPL',
+    url = 'https://github.com/grburgess/3ml_utils',
+    packages=['3ml_utils'],
+    requires = ['matplotlib', 'numpy', 'threeML']
+
+
+
+
+)
